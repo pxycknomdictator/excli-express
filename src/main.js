ea9f576794c1c268c4f7bea68a11a24293721f34
Noman <pxycknomdictator@outlook.com>
2025-06-12 15:06:39 +0500

    feat: reassemble all cli code

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import { cwd } from "node:process";
 import { join, basename } from "node:path";
 import { existsSync, mkdirSync, cpSync, writeFileSync } from "node:fs";
-import { intro, text, select, multiselect, isCancel } from "@clack/prompts";
+import { intro, text, select } from "@clack/prompts";
+import { multiselect, isCancel, cancel } from "@clack/prompts";
 import { directories, git_configs, prettierConfigs } from "./options.js";
 import { database, terminate } from "./utils.js";
 
@@ -13,15 +14,17 @@ intro("🔥 Express.js App Generator | Build your dreams, faster! ⚡");
     message: "What should we name your server directory? 🎯",
     placeholder: "server (Hit Enter for current directory)",
   });
-
   if (isCancel(directory)) terminate("Process cancelled ❌");
 
   const rootDir = cwd();
   const targetDir = !directory?.trim() ? rootDir : join(rootDir, directory);
   const dirName = basename(targetDir) || "container_app";
 
-  const condition = directory !== undefined && !existsSync(targetDir);
-  if (condition) mkdirSync(targetDir, { recursive: true });
+  if (existsSync(targetDir) && directory?.trim()) {
+    return terminate(
+      `${directory} already exists. Please choose a different name 🤷`,
+    );
+  }
 
   const language = await select({
     message: "Pick your coding poison:",
@@ -33,23 +36,6 @@ intro("🔥 Express.js App Generator | Build your dreams, faster! ⚡");
 
   if (isCancel(language)) terminate("Process cancelled ❌");
 
-  const sourceDir = join(targetDir, "src");
-
-  if (!existsSync(sourceDir)) {
-    const publicDir = join(targetDir, "public");
-    const template = join(rootDir, "templates", language);
-
-    mkdirSync(publicDir, { recursive: true });
-    cpSync(template, targetDir, { recursive: true });
-  }
-
-  for (let dir of directories) {
-    if (language !== "ts" && dir === "types") continue;
-
-    const directory = join(sourceDir, dir);
-    mkdirSync(directory, { recursive: true });
-  }
-
   const devTools = await multiselect({
     message: "🛠️ Setting up core development tools...",
     options: [
@@ -58,10 +44,13 @@ intro("🔥 Express.js App Generator | Build your dreams, faster! ⚡");
       { label: "🔨 Git", value: "git" },
     ],
   });
+
   if (isCancel(devTools)) terminate("Process cancelled ❌");
 
+  let db = null;
+
   if (devTools.includes("docker")) {
-    const db = await select({
+    db = await select({
       message: "Alright, pick your poison",
       options: [
         { label: "🐬 MySQL", value: "mysql", hint: "Old reliable 🛠️" },
@@ -69,10 +58,26 @@ intro("🔥 Express.js App Generator | Build your dreams, faster! ⚡");
         { label: "🍃 MongoDB", value: "mongodb", hint: "Flexible chaos ✨" },
       ],
     });
-
     if (isCancel(db)) terminate("Process cancelled ❌");
-    const config = database(db, dirName);
+  }
 
+  if (!existsSync(targetDir)) mkdirSync(targetDir, { recursive: true });
+
+  const sourceDir = join(targetDir, "src");
+  const publicDir = join(targetDir, "public");
+  const template = join(rootDir, "templates", language);
+
+  mkdirSync(publicDir, { recursive: true });
+  cpSync(template, targetDir, { recursive: true });
+
+  for (let dir of directories) {
+    if (language !== "ts" && dir === "types") continue;
+    const directoryPath = join(sourceDir, dir);
+    mkdirSync(directoryPath, { recursive: true });
+  }
+
+  if (devTools.includes("docker") && db) {
+    const config = database(db, dirName);
     if (config?.trim()) {
       const docker_compose = join(targetDir, "compose.yaml");
       writeFileSync(docker_compose, config);
@@ -82,13 +87,11 @@ intro("🔥 Express.js App Generator | Build your dreams, faster! ⚡");
   if (devTools.includes("git")) {
     const gitPath = join(targetDir, ".gitignore");
     const { gitignoreContent } = git_configs();
-
     writeFileSync(gitPath, gitignoreContent);
   }
 
   if (devTools.includes("prettier")) {
     const configurations = prettierConfigs();
-
     for (let config of configurations) {
       const configs = join(targetDir, config.filename);
       writeFileSync(configs, config.content);
