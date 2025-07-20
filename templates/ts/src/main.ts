2e7f39b3e2f031b134ff48c41e294a64d742e01a
Noman <pxycknomdictator@outlook.com>
2025-07-20 22:51:36 +0500

    add new formate of express app

--- templates/ts/src/main.ts
+++ templates/ts/src/main.ts
@@ -1,10 +1,28 @@
-import { createServer } from "node:http";
-import { app } from "@/app.js";
-import { configs } from "@/constant.js";
+import cors from "cors";
+import helmet from "helmet";
+import http from "node:http";
+import path from "node:path";
+import express from "express";
+import process from "node:process";
+import type { Request, Response } from "express";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+const server = http.createServer(app);
+const client = process.env.CLIENT_ORIGIN || "*";
+const staticFiles = path.join(process.cwd(), "public");
+
+app.use(helmet());
+app.use(express.static(staticFiles));
+app.use(express.json({ limit: "20mb" }));
+app.use(cors({ origin: client, credentials: true }));
+app.use(express.urlencoded({ limit: "20mb", extended: true }));
+
+app.get("/", (_: Request, res: Response) => {
+    return res.status(200).send("<h1>Thanks for using Excli</h1>");
+});
 
 // FIRE IN THE HOLE
-(async () => {
-    const PORT = configs.PORT || 3000;
-    const server = createServer(app);
+(() => {
     server.listen(PORT, () => console.log(`Express: http://localhost:${PORT}`));
 })();
