6a9ad220f18d85e55a3c603eb003dc0b468f802e
Noman <pxycknomdictator@outlook.com>
2026-01-09 02:45:08 +0500

    add proper types in function parameter

--- src/utils/shell.ts
+++ src/utils/shell.ts
@@ -1,7 +1,8 @@
 import { platform } from "node:process";
 import { spawnSync, spawn } from "node:child_process";
+import type { PackageManager } from "@/types";
 
-export function hasPkManager(pkgM: string) {
+export function hasPkManager(pkgM: PackageManager) {
     const command = platform !== "win32" ? "which" : "where";
     const result = spawnSync(command, [pkgM], { encoding: "utf-8" });
     return result.status === 0;
