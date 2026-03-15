e1b93b132c93cf25fc1783e5bb8f264b1691db5b
Noman <pxycknomdictator@outlook.com>
2026-03-15 23:21:29 +0500

    refactor: change main server file execution

--- templates/js/src/main.js
+++ templates/js/src/main.js
@@ -1,10 +1,10 @@
 import http from "node:http";
 import { app } from "./app.js";
-import { env } from "./constant.js";
 
-const PORT = env.PORT || 3000;
-const server = http.createServer(app);
-
-(() => {
+async function bootstrap() {
+    const PORT = process.env.PORT ?? 3000;
+    const server = http.createServer(app);
     server.listen(PORT, () => console.log(`Express: http://localhost:${PORT}`));
-})();
+}
+
+bootstrap();
