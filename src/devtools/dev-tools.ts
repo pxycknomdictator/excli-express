869eaef9cf45630ad9cbbf32bb53b0b8f89e0116
Noman <pxycknomdictator@outlook.com>
2026-03-24 13:51:08 +0500

    refactor: initialize dev tools fast

--- src/devtools/dev-tools.ts
+++ src/devtools/dev-tools.ts
@@ -10,18 +10,17 @@ import type { ProjectConfig } from "src/types";
 export async function setupDevTools(config: ProjectConfig) {
     const { devTools, targetDir } = config;
 
+    if (devTools.includes("prettier")) await setupPrettier(targetDir);
     if (devTools.includes("git") && hasGit()) {
         await Promise.all([
             fireShell("git init", targetDir),
             setupGit(targetDir),
         ]);
     }
-    await installPackages(config);
-
-    if (devTools.includes("prettier")) await setupPrettier(targetDir);
-    if (devTools.includes("husky")) await setupHusky(targetDir);
     if (devTools.includes("docker")) {
         await setupDocker(config);
         await setupOrm(config);
     }
+    await installPackages(config);
+    if (devTools.includes("husky")) await setupHusky(targetDir);
 }
