1ae757df17ad5e081ec0e4c6f2f308632bb2a703
Noman <pxycknomdictator@outlook.com>
2026-03-28 00:41:37 +0500

    fix: remove unnecessary code from codebase

--- src/generators/env.ts
+++ src/generators/env.ts
@@ -2,19 +2,14 @@ import { join } from "node:path";
 import { writeFile } from "node:fs/promises";
 import { envConfig } from "src/config";
 
-function getEnvVariables(): Record<string, string> {
-    return envConfig;
-}
-
 function formatEnvWithComments(): { env: string; envExample: string } {
-    const vars = getEnvVariables();
     const envLines: string[] = [];
     const exampleEnvLines: string[] = [];
 
     envLines.push("# Application Configuration");
-    envLines.push(`NODE_ENV=${vars.NODE_ENV}`);
-    envLines.push(`PORT=${vars.PORT}`);
-    envLines.push(`CLIENT_ORIGIN=${vars.CLIENT_ORIGIN}\n`);
+    envLines.push(`NODE_ENV=${envConfig.NODE_ENV}`);
+    envLines.push(`PORT=${envConfig.PORT}`);
+    envLines.push(`CLIENT_ORIGIN=${envConfig.CLIENT_ORIGIN}\n`);
 
     exampleEnvLines.push("# Application Configuration");
     exampleEnvLines.push("NODE_ENV=");
