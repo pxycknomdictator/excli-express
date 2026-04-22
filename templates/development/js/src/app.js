801b44f42711d5604483e52063816426ffe4a83e
Noman <pxycknomdictator@outlook.com>
2026-04-23 03:52:40 +0500

    update: response object from the home route

--- templates/development/js/src/app.js
+++ templates/development/js/src/app.js
@@ -6,7 +6,9 @@ app.use(express.json({ limit: "20mb" }));
 app.use(express.urlencoded({ limit: "20mb", extended: true }));
 
 app.get("/", (_, res) => {
-    return res.status(200).send("<h1>Thanks for using Express Cli</h1>");
+    return res
+        .status(200)
+        .json({ success: true, message: "Thanks for using @excli/express" });
 });
 
 export { app };
--- templates/development/ts/src/app.ts
+++ templates/development/ts/src/app.ts
@@ -7,7 +7,9 @@ app.use(express.json({ limit: "20mb" }));
 app.use(express.urlencoded({ limit: "20mb", extended: true }));
 
 app.get("/", (_: Request, res: Response) => {
-    return res.status(200).send("<h1>Thanks for using Express Cli</h1>");
+    return res
+        .status(200)
+        .json({ success: true, message: "Thanks for using @excli/express" });
 });
 
 export { app };
