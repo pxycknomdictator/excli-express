1a58b65f09667295971a76da0f8af8abc3c5e8ec
Noman <pxycknomdictator@outlook.com>
2026-05-23 01:31:58 +0500

    feat: add better auth prisma support

--- src/auth/better-auth.ts
+++ src/auth/better-auth.ts
@@ -9,7 +9,15 @@ const betterAuthDrizzleProvider = {
     sqlite: "sqlite",
 };
 
-function drizzleBetterAuthSQL(config: ProjectConfig) {
+const betterAuthPrismaProvider = {
+    mysql: "mysql",
+    mariadb: "mysql",
+    postgres: "postgresql",
+    sqlite: "sqlite",
+    mongodb: "mongodb",
+};
+
+function drizzleBetterAuth(config: ProjectConfig) {
     const sqlDatabase = config.database as SQL_DATABASE;
     const provider = betterAuthDrizzleProvider[sqlDatabase];
 
@@ -27,12 +35,35 @@ export const auth = betterAuth({
 `;
 }
 
+function prismaBetterAuth(config: ProjectConfig) {
+    const database = config.database as ProjectConfig["database"];
+    const provider = betterAuthPrismaProvider[database!];
+
+    return `import { prisma } from "../db/index.js";
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+
+export const auth = betterAuth({
+    appName: "${config.directory}",
+    baseURL: process.env.BETTER_AUTH_URL,
+    secret: process.env.BETTER_AUTH_SECRET,
+    emailAndPassword: { enabled: true },
+    database: prismaAdapter(prisma, { provider: "${provider}", usePlural: true }),
+});
+`;
+}
+
 export async function setupBetterAuth(config: ProjectConfig) {
     const authName = join(config.targetDir, "src", "lib", "auth");
     const [authFile] = appendLanguageExtension(config.language, authName);
 
-    if (config.databaseType === "sql" && config.databaseOrm === "drizzle") {
-        const auth = drizzleBetterAuthSQL(config);
+    if (config.databaseOrm === "drizzle") {
+        const auth = drizzleBetterAuth(config);
+        await generateFile({ fileLocation: authFile!, fileContent: auth });
+    }
+
+    if (config.databaseOrm === "prisma") {
+        const auth = prismaBetterAuth(config);
         await generateFile({ fileLocation: authFile!, fileContent: auth });
     }
 }
