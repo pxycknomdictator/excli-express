9a2ab205c4d4ccebd2ec1ac9304ee1690bbf7367
Noman <pxycknomdictator@outlook.com>
2026-06-24 03:25:51 +0500

    catch all auth routes in express server

--- templates/production/js/src/app.js
+++ templates/production/js/src/app.js
@@ -6,8 +6,10 @@ import helmet from "helmet";
 import express from "express";
 import { rateLimit } from "express-rate-limit";
 
+import { auth } from "./lib/auth.js";
 import { corsOptions, globalLimiter } from "./constant.js";
 import { globalErrorWrapper } from "./utils/global.js";
+import { toNodeHandler } from "better-auth/node";
 
 const app = express();
 const limiter = rateLimit(globalLimiter);
@@ -19,6 +21,7 @@ app.use(helmet());
 app.use(express.static(staticFiles));
 app.use(express.json({ limit: "20mb" }));
 app.use(express.urlencoded({ limit: "20mb", extended: true }));
+app.all("/api/auth/*splat", toNodeHandler(auth));
 
 import { healthRouter } from "./routes/health.js";
 
--- templates/production/ts/src/app.ts
+++ templates/production/ts/src/app.ts
@@ -7,8 +7,10 @@ import express from "express";
 import type { Express } from "express";
 import { rateLimit } from "express-rate-limit";
 
+import { auth } from "./lib/auth.js";
 import { corsOptions, globalLimiter } from "./constant.js";
 import { globalErrorWrapper } from "./utils/global.js";
+import { toNodeHandler } from "better-auth/node";
 
 const app: Express = express();
 const limiter = rateLimit(globalLimiter);
@@ -20,6 +22,7 @@ app.use(helmet());
 app.use(express.static(staticFiles));
 app.use(express.json({ limit: "20mb" }));
 app.use(express.urlencoded({ limit: "20mb", extended: true }));
+app.all("/api/auth/*splat", toNodeHandler(auth));
 
 import { healthRouter } from "./routes/health.js";
 
