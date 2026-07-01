6ee8bf51437b5a995eaa346969509aee3c317e4e
Noman <pxycknomdictator@outlook.com>
2026-07-02 04:08:31 +0500

    fix static file paths error

--- templates/production/js/src/app.better.auth.js
+++ templates/production/js/src/app.better.auth.js
@@ -1,5 +1,5 @@
-import path from "node:path";
-import process from "node:process";
+import { dirname, join } from "node:path";
+import { fileURLToPath } from "node:url";
 
 import cors from "cors";
 import helmet from "helmet";
@@ -13,12 +13,13 @@ import { toNodeHandler } from "better-auth/node";
 
 const app = express();
 const limiter = rateLimit(globalLimiter);
-const staticFiles = path.join(process.cwd(), "public");
+const fileRoute = dirname(fileURLToPath(import.meta.url));
+const staticAssets = join(fileRoute, "..", "public");
 
 app.use(cors(corsOptions));
 app.use(limiter);
 app.use(helmet());
-app.use(express.static(staticFiles));
+app.use(express.static(staticAssets));
 app.use(express.json({ limit: "20mb" }));
 app.use(express.urlencoded({ limit: "20mb", extended: true }));
 app.all("/api/auth/*splat", toNodeHandler(auth));
--- templates/production/js/src/app.normal.js
+++ templates/production/js/src/app.normal.js
@@ -1,5 +1,5 @@
-import path from "node:path";
-import process from "node:process";
+import { dirname, join } from "node:path";
+import { fileURLToPath } from "node:url";
 
 import cors from "cors";
 import helmet from "helmet";
@@ -11,12 +11,13 @@ import { globalErrorWrapper } from "./utils/global.js";
 
 const app = express();
 const limiter = rateLimit(globalLimiter);
-const staticFiles = path.join(process.cwd(), "public");
+const fileRoute = dirname(fileURLToPath(import.meta.url));
+const staticAssets = join(fileRoute, "..", "public");
 
 app.use(cors(corsOptions));
 app.use(limiter);
 app.use(helmet());
-app.use(express.static(staticFiles));
+app.use(express.static(staticAssets));
 app.use(express.json({ limit: "20mb" }));
 app.use(express.urlencoded({ limit: "20mb", extended: true }));
 
